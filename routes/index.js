d4adf0bf2e7272499e82ca54afb0217925d082c0
Viet Anh Nguyen <nva1308@gmail.com>
2019-08-23 15:11:31 +0700

    complete next question and calculate score. all saved to db

--- routes/index.js
+++ routes/index.js
@@ -1,90 +1,103 @@
 var express = require('express');
-var sqlite3 = require('sqlite3').verbose();
-
 var router = express.Router();
+var utils = require("./utils")
 
-var db = new sqlite3.Database('./routes/aws_question (1).sqlite');
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	db.all('SELECT tbl_answer.is_correct isCorrect, tbl_question.id quesID, tbl_answer.id ansID, tbl_question.content questionContent, tbl_answer.content answerContent FROM tbl_answer, tbl_question WHERE tbl_question.id = tbl_answer.question_id AND question_set=?', [20], (err, row) => {
-		if (err) {
-			return console.log(err.message);
+router.use('/quiz/:testId/:setId', async (req, res, next) => {
+	console.log('method' + req.method)
+	let setId = req.params.setId
+	let testId = req.params.testId
+	let prevQuestionId = [];
+	if (req.method === 'GET') {
+		// load the 1st question and answer in set
+		let queryFirstQuestion = `select * from tbl_question where question_set = ${setId} LIMIT 1`
+		let firstQuestion = await utils.selectDB(queryFirstQuestion)
+		let questionId = parseInt(firstQuestion[0]['id'])
+		prevQuestionId.push(questionId)
+		let getListAnswers = `select * from tbl_answer where question_id = ${questionId}`
+		let answersBelong = await utils.selectDB(getListAnswers);
+		res.render('index', { testId: testId, questionSet: setId, question: firstQuestion[0], answers: answersBelong });
+	}
+	if (req.method === 'POST') {
+		// get list answer after student choose
+		let answerPicked1 = req.body.ans;
+		let answerPicked = [];
+		if (typeof (answerPicked1) === 'string') {
+			answerPicked.push(parseInt(answerPicked1));
+		} else {
+			answerPicked = answerPicked1;
 		}
-		// res.render('index', { data: row });
-	})
+		// get list is_correct of list answer
+		let isTrue = [];
+		let currentQuestionId = req.body.questionId;
+		let getCurrentListAnswers = `select * from tbl_answer where question_id = ${currentQuestionId}`
+		let currentAnswersBelong = await utils.selectDB(getCurrentListAnswers);
+		for (let i = 0; i < currentAnswersBelong.length; i++) {
+			isTrue.push(currentAnswersBelong[i].is_correct);
+		}
+		// save student's answer to tbl_choice
+		for (let i = 0; i < answerPicked.length; i++) {
+			let queryChoice = `insert into tbl_choice (test_id, question_id, answer_id, is_correct) values(${testId}, ${currentQuestionId}, ${answerPicked[i]}, ${isTrue[i]})`;
+			await utils.runDB(queryChoice);
+		}
+		// return next unanswer question
+		let queryNextQuestion =
+			`select *
+				from tbl_question
+				where question_set = ${setId}  
+				and id not in (select question_id from tbl_choice where test_id=${testId})
+			limit 1`
+		let nextQuestion = await utils.selectDB(queryNextQuestion);
+		if (nextQuestion.length != 0) {
+			let nextQuestionId = parseInt(nextQuestion[0]['id'])
+			prevQuestionId.push(nextQuestionId)
+			let getNextListAnswers = `select * from tbl_answer where question_id = ${nextQuestionId}`
+			let nextAnswersBelong = await utils.selectDB(getNextListAnswers)
+			res.render('index', { prevQuestionId: currentQuestionId, testId: testId, questionSet: setId, question: nextQuestion[0], answers: nextAnswersBelong });
+		} else {
+			// if the current question is end of set, then calculate score
+			let queryCountFalseQuestion = `SELECT COUNT(question_id) as num_wrong_ans
+											FROM (SELECT DISTINCT question_id FROM tbl_choice WHERE is_correct = 0 and test_id=${testId})`;
+			let countFalseQuestion = await utils.selectDB(queryCountFalseQuestion);
+			let queryTotalQuestion = `select count(id) as total_question from tbl_question where question_set=${setId}`
+			let totalQuestion = await utils.selectDB(queryTotalQuestion);
+			let countTrueQuestion = totalQuestion[0].total_question - countFalseQuestion[0].num_wrong_ans;
+			let truePercent = (countTrueQuestion / totalQuestion[0].total_question).toFixed(4) * 100;
+			if (truePercent >= 70) {
+				let queryUpdateTest = `update tbl_test set status='true', score=${truePercent}, result='passed' where id=${testId}`;
+				await utils.runDB(queryUpdateTest);
+				res.send('passed: ' + truePercent + '%');
+			} else {
+				let queryUpdateTest = `update tbl_test set status='false', score=${truePercent}, result='failed' where id=${testId}`;
+				await utils.runDB(queryUpdateTest);
+				res.send('failed: ' + truePercent + '%');
+			}
+		}
+	}
+
 });
 
-// router.get('/:questionSet', function (req, res, next) {
-// 	let allData = [];
-// 	let getOnlyOneQuestion = [];
-// 	let i = 0;
-// 	let questionSet = req.params.questionSet;
-// 	let questionPromise = new Promise((resolve, reject) => {
-// 		db.all('SELECT id, content FROM tbl_question WHERE question_set = ?', [questionSet], (err, rows) => {
-// 			getOnlyOneQuestion = rows;
-// 			resolve(getOnlyOneQuestion);
-// 		})
-// 	})
-// 	let getAllPromise = new Promise((resolve, reject) => {
-// 		db.all('SELECT tbl_answer.is_correct isCorrect, tbl_question.id quesID, tbl_answer.id ansID, tbl_question.content questionContent, tbl_answer.content answerContent FROM tbl_answer, tbl_question WHERE tbl_question.id = tbl_answer.question_id AND question_set=?', [questionSet], (err, rows) => {
-// 			allData = rows;
-// 			resolve(allData);
-// 		})
-// 	})
-// 	getAllPromise.then((result) => {
-// 		res.render('index', { questionSet: questionSet, i: i, getOnlyOneQuestion: getOnlyOneQuestion, allData: allData });
-// 	})
-// })
-
-router.get('/:questionSet/:iVariable', function (req, res, next) {
-	let allData = [];
-	let getOnlyOneQuestion = [];
-	let i = parseInt(req.params.iVariable);
-	let questionSet = req.params.questionSet;
-	console.log('questionSet: ' + questionSet + ', i: ' + i);
-	let questionPromise = new Promise((resolve, reject) => {
-		db.all('SELECT id, content FROM tbl_question WHERE question_set = ?', [questionSet], (err, rows) => {
-			getOnlyOneQuestion = rows;
-			resolve(getOnlyOneQuestion);
-		})
-	})
-	let getAllPromise = new Promise((resolve, reject) => {
-		db.all('SELECT tbl_answer.is_correct isCorrect, tbl_question.id quesID, tbl_answer.id ansID, tbl_question.content questionContent, tbl_answer.content answerContent FROM tbl_answer, tbl_question WHERE tbl_question.id = tbl_answer.question_id AND question_set=?', [questionSet], (err, rows) => {
-			allData = rows;
-			resolve(allData);
-		})
-	})
-	getAllPromise.then((result) => {
-		res.render('index', { questionSet: questionSet, i: i, getOnlyOneQuestion: getOnlyOneQuestion, allData: allData });
-	})
+router.use('/take-quiz/:setId', async (req, res, next) => {
+	// create a new record in table tbl_test
+	let setId = req.params.setId
+	// get test_id just created
+	let queryTest = `insert into tbl_test (user_id, status, score, result, start_time) values(1, 'false', 0, 0, DATETIME())`;
+	let testId = await utils.runDB(queryTest);
+	console.log('testId: ' + testId);
+	res.redirect("/quiz/" + testId + "/" + setId)
 })
 
-router.post('/:testId/:questionId/:answerId/:isCorrect', function (req, res, next) {
-	let testId = req.params.testId;
-	let questionId = req.params.questionId;
-	let answerId = req.params.answerId;
-	let temp = req.params.isCorrect;
-	console.log('temp: ', temp);
-	let isCorrect;
-	if (temp == 1) {
-		isCorrect = true;
-	} else {
-		isCorrect = false;
+router.use('/', async (req, res, next) => {
+	let querySet = 'select distinct question_set from tbl_question order by question_set';
+	let listSet = await utils.selectDB(querySet);
+	let htmlRender = "";
+	for (let i = 0; i < listSet.length; i++) {
+		htmlRender += `<a href="/take-quiz/${listSet[i].question_set}">set ${listSet[i].question_set}</a><br>`;
 	}
-	// let data = [];
-	// db.each('SELECT id FROM tbl_question', (err, row) => {
-	// 	if (err) {
-	// 		return console.log(err.message);
-	// 	}
-	db.run('INSERT INTO tbl_choice (test_id, question_id, answer_id, is_correct)  VALUES(?, ?, ?, ?)', [testId, questionId, answerId, isCorrect], (err) => {
-		if (err) {
-			return console.log(err.message);
-		}
-		// res.render('index', { data: row });
-	})
-	// })
-});
+	res.send(htmlRender);
+
+})
 
 module.exports = router;
---
+++ routes/utils.js
@@ -0,0 +1,31 @@
+var sqlite3 = require('sqlite3').verbose();
+
+
+const selectDB = (query) => {
+	let db = new sqlite3.Database('./routes/aws_question.sqlite');
+	return new Promise((resolve, reject) => {
+		db.all(query, (err, rows) => {
+			if (err) {
+				console.log(err)
+			}
+			resolve(rows);
+		})
+		db.close();
+	})
+}
+
+const runDB = (query) => {
+	let db = new sqlite3.Database('./routes/aws_question.sqlite');
+	return new Promise((resolve, reject) => {
+		db.run(query, function (err) {
+			if (err) {
+				console.log(err)
+			}
+			resolve(this.lastID);
+		})
+		db.close();
+	})
+}
+
+exports.selectDB = selectDB
+exports.runDB = runDB
